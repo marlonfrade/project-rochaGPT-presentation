d818464cc9e65d716b48822563fc7ce11a635bc8
Marlon Frade <marlonmelo98@hotmail.com>
2023-04-26 16:42:24 -0300

    docs(*): initial commit docs and lint adjust

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,25 @@ module.exports = {
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
     },
+  },
+  corePlugins: {
+    fontSize: false,
   },
   plugins: [require("@tailwindcss/typography")],
 };
