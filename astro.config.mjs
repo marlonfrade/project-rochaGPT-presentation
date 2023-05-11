f63c23f464f1a000cb7985281e775fd0e97fc1a4
Marlon Frade <marlonmelo98@hotmail.com>
2023-05-11 11:41:32 -0300

    fix(config): fix share media content url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://rochagpt.ai",
+  site: "https://rochagpt.netlify.app/",
   integrations: [
     tailwind(),
     image({
